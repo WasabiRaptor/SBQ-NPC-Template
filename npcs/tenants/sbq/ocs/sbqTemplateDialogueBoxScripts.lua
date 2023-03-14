773515c4b8a4bcc45a9144b0f33f407df05ede07
WasabiRaptor <ketchupraptor@gmail.com>
2023-03-14 13:49:51 -0400

    update the args here

--- npcs/tenants/sbq/ocs/sbqTemplateDialogueBoxScripts.lua
+++ npcs/tenants/sbq/ocs/sbqTemplateDialogueBoxScripts.lua
@@ -1,16 +1,18 @@
 ---@diagnostic disable: undefined-global
 
 -- this is an example function with the args a script would recieve from being used as a "next" step
-function dialogueBoxScripts.exampleNextStep(dialogueTree, settings, branch)
+function dialogueBoxScripts.exampleNextStep(dialogueTree, settings, branch, eid, ...)
 
 
-	return dialogueTree -- the dialogueTree must be returned at the end of the function
+	return dialogueTree.default -- the dialogueTree must be returned at the end of the function
+	-- when you return the dialogue tree, return the next branch you want to go down
 end
 
 -- this is an example function with the args a script would recieve from being used as a "callScript" when clicking the continue button after finishing a dialogue
 -- the "..." args are from the "scriptArgs" table from your current position in the dialogue tree when the function is called
-function dialogueBoxScripts.exampleCallScript(dialogueTree, settings, ...)
+function dialogueBoxScripts.exampleCallScript(dialogueTree, settings, branch, eid, ...)
 
 
-	return dialogueTree -- the dialogueTree must be returned at the end of the function
+	return dialogueTree.default -- the dialogueTree must be returned at the end of the function
+	-- when you return the dialogue tree, return the next branch you want to go down
 end
